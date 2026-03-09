76a1825e795173501b30576a057f10d5b310595e
Annete Montserrat Cedillo Mariscal <a01638984@tec.mx>
2026-03-09 08:04:08 -0600

    Add functional navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Router:	Envuelve la aplicación y gestiona la navegación.
+//Routes:	Agrupa múltiples rutas y muestra solo la primera coincidencia.
+//Route:	Define una URL específica y qué componente renderizar.
 
-function App() {
-  const [count, setCount] = useState(0)
+//Instala lo siguiente
+//npm install sockjs-client stompjs
 
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./pages/NavBar/Navbar";
+import Dashbord from "./pages/mainPages/Dashboard";
+import About from "./pages/mainPages/About";
+import Home from "./pages/mainPages/Home";
+
+function Default() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div style={{ padding: 32 }}>
+      <h1>404 — Página no encontrada</h1>
+      <p>
+        Regresa a <a href="/">Home</a>.
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        {/* Barra de navegación */}
+        <Navbar />
+        {/* Contenido de la página */}
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/dashbord" element={<Dashbord />} />
+            <Route path="*" element={<Default />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
